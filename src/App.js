261f99663a16f1dfd72eb5e5f252b8629bd8a320
Arjun <jadhavarjun53@gmail.com>
2021-05-18 15:37:35 +0530

    [Arjun] Refactored: remove functional component and implement class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-    <h1>Todo List</h1>
-    </div>
-  );
+import react,{Component} from 'react';
+class App extends Component{
+  render(){
+    return (
+      <div className="container-fluid my-5">
+        <div className="row">
+          <div className="col-sm-6 mx-auto text-white shadow-lg">
+            <h1 className="text-center">
+              Todays Paln's
+            </h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
